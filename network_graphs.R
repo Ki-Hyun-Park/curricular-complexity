f209dd1a58b17387f406cbfafb1dc7493245030b
Jeremy Tran <jrtran@ucla.edu>
2020-03-02 15:33:21 -0800

    adding stuff for network graphs

--- network_graphs.R
+++ network_graphs.R
@@ -22,3 +22,29 @@ vis.links$arrows <- "middle"
 
 visnet <- visNetwork(vis.nodes, vis.links)
 visnet
+
+
+# a slightly different view for a single course
+nodes <- tibble(
+  id = c(1, 2, 3, 4, 5, 6, 7, 8),
+  label = c(
+    "MATH 33A", "1 of 3", "MATH 3B", "MATH 31B", "MATH 32A",
+    "MATH 3A", "MATH 31A", "MATH 1"
+  )
+  # value = ,
+  # title = ,
+)
+
+edges <- tibble(
+  from = c(1, 2, 2, 2, 3, 6, 4, 7, 5),
+  to = c(2, 3, 4, 5, 6, 8, 7, 8, 7)
+)
+
+vis.nodes <- nodes
+vis.links <- edges
+vis.links$arrows <- "to"
+vis.nodes$color.background <- c("white", "yellow", rep("white", 6))
+vis.nodes$shape <- "circle"
+
+visnet <- visNetwork(vis.nodes, vis.links)
+visnet
