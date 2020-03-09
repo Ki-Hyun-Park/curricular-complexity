e00d594565515351971143c7214b289215761ed2
manthedan28 <50038688+manthedan28@users.noreply.github.com>
2020-03-08 23:23:29 -0700

    Update network_graphs_TEST.R
    added in curriculum maps for econ and nursing majors

--- network_graphs_TEST.R
+++ network_graphs_TEST.R
@@ -76,7 +76,7 @@ visnet %>%
 #   visOptions(highlightNearest = TRUE, collapse = TRUE) %>%
 #   visHierarchicalLayout(direction = "UD")
 
-# 3. Fixing POLISCI and Nursing
+# 3. Fixing POLISCI
 extract_level <- function(course_vec){
   return(as.integer(str_extract(course_vec, "\\d+")) %/% 100 + 1)
 }
@@ -108,3 +108,76 @@ visnet <- visNetwork(vis.nodes, vis.links, main = "POL SCI Major")
 visnet %>% 
   visOptions(highlightNearest = TRUE, collapse = TRUE) %>%
   visHierarchicalLayout(direction = "UD")
+
+### ECON AND NURSING
+##### ECON 
+library(stringr)
+
+color_vec <- c('lightblue', 'yellow')
+
+label <- c("ECON 1", "ECON  2", "ECON 11", "ECON 41", "MATH 31A", 
+          "MATH 31B", "WRITING II", "ECON 101", "ECON 102", 
+          "ECON 103/103L", "ECON 134", "ECON 137", "ECON 145", 
+          "ECON 160", "ECON 106G", "ECON 195")
+
+extract_level <- function(course_vec){
+  return(as.integer(str_extract(course_vec, "\\d+")) %/% 100 + 1)
+}
+
+extract_level
+
+vis.nodes <- tibble(
+  id = 1:length(label),
+  label = label,
+  shape = "circle",
+  color.background = color_vec[extract_level(label)],
+  color.border = "black",
+  shadow = TRUE
+)
+
+vis.links <- tibble(
+  from = c(1, 2, 3, 3, 3,4 , 4 , 5, 6, 7, 8, 8 , 8,  8,  8,  9),
+  to =   c(3, 3, 8, 12, 10, 11, 10, 6, 4, 7, 9, 11, 13, 15, 16,  14 ),
+  color = "black",
+  arrows = "to"
+)
+
+
+visnet <- visNetwork(vis.nodes, vis.links, main = "ECONOMICS Major")
+visnet %>% 
+  visOptions(highlightNearest = TRUE, collapse = TRUE) %>%
+  visHierarchicalLayout(direction = "UD")
+
+########### BNURSING 
+
+color_vec <- c('lightblue', 'yellow')
+
+label <- c("Chem 14A", "Chem14B", "Nursing20", "Nursing10", "Nursing54B", "Nursing54A", "Nursing115", "Life Sci 7A", "Nursing 150A", "Nursing 150B", "Nursing 152A", "Nursing 152B", "Nursing 161", "Nursing 162B", "Nursing 162A", "Nursing 54C", "Nursing 162C", "Nursing 162D", "Nursing 163", "Nursing 164","Nursing 165", "Nursing 168","Nursing 171", "Nursing 16","Chem 14C","Nursing 160","Nursing 160B","Nursing 161C","Nursing 174","Nursing 3", "Nursing 13")
+
+extract_level <- function(course_vec){
+  return(as.integer(str_extract(course_vec, "\\d+")) %/% 100 + 1)
+}
+
+extract_level
+
+vis.nodes <- tibble(
+  id = 1:length(label),
+  label = label,
+  shape = "circle",
+  color.background = color_vec[extract_level(label)],
+  color.border = "black",
+  shadow = TRUE
+)
+
+vis.links <- tibble(
+  from = c(25,25,3,5,7,7,7,9,9,9,10,10,10,10,26,26,26,26,13,13,15,15,15,14,17,18,19,20,20,21,22,22,22,22,22,23,23,23,23,23,29,24,24,24,24,24),
+  to =   c(1,2,4,6,6,5,6,8,4,3,6,9,11,12,23,9,10,11,12,26,14,6,5,16,15,14,17,15,26,27,26,14,13,19,20,21,28,13,19,20,21,28,30,31,13,19),
+  color = 'black',
+  arrows = 'to'
+)
+
+
+visnet <- visNetwork(vis.nodes, vis.links, main = "NURSING Major")
+visnet %>% 
+  visOptions(highlightNearest = TRUE, collapse = TRUE) %>%
+  visHierarchicalLayout(direction = "LR")
